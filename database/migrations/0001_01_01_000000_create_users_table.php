63139f5e78691dd0c4b05229a0221a7298f1305c
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-09-07 13:57:46 +0300

    Update users table migration to make password nullable and add additional fields

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->enum('role', ['Owner', 'HR','Manager','Employee'])->default('Employee');
+            $table->string('avatar')->nullable();
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
