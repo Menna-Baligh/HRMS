64da61e31d42dfe86781719b5344514117bcc0cb
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-09-07 18:06:28 +0300

    routes(api): add authentication login route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+        Route::post('/login', [AuthController::class,'login'])
+            ->middleware('throttle:5,1');
+    });
 
 
