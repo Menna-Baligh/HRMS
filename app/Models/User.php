63c0792c0803c521fd2158c70e6d7379371482af
alsayed salem <alsayedsalem@gmail.com>
2026-09-09 10:12:14 +0300

    handed regiter and reset password

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,15 +7,28 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'role',
+        'avatar',
+        'provider',
+        'provider_id',
+        'password'
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +41,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::prefix('auth')->group(function () {
+    // Route::post('/login', [AuthController::class,'login'])
+    //     ->middleware('throttle:5,1');
+    Route::post('/register',[AuthController::class, 'register'])->middleware('throttle:5,1');
+    Route::post('/forget-password',[AuthController::class, 'forgotPassword'])->middleware('throttle:5,1');
+    Route::post('/forgot-password/verify-otp',[AuthController::class, 'verifyForgotPasswordOtp'])->middleware('throttle:5,1');
+    Route::post('/forgot-password/reset',[AuthController::class, 'resetPassword'])->middleware('throttle:5,1');
+    Route::post('/forgot-password/resend-otp',[AuthController::class, 'resendOtp'])->middleware('throttle:5,1');
+});
