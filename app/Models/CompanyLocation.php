3dba663ad27e9befba39235f3bbaa23992152685
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-09-12 14:37:11 +0300

    feat(attendance): setup attendance schema and model relationships

--- app/Models/CompanyLocation.php
+++ app/Models/CompanyLocation.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CompanyLocation extends Model
 {
@@ -23,5 +24,9 @@ class CompanyLocation extends Model
         'is_active' => 'boolean',
     ];
 
+    public function attendances(): HasMany
+    {
+        return $this->hasMany(Attendance::class);
+    }
 
 }
