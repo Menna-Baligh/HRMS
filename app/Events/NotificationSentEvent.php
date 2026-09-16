c199a85b197c5982b992b89a0dcdd7f8986d961e
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-09-16 22:33:18 +0300

    enhance NotificationResource and EmployeeResource with locale support

--- app/Events/NotificationSentEvent.php
+++ app/Events/NotificationSentEvent.php
@@ -4,14 +4,13 @@ namespace App\Events;
 
 use App\Http\Resources\NotificationResource;
 use App\Models\Notification;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationSentEvent implements ShouldBroadcast
+class NotificationSentEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -39,11 +38,10 @@ class NotificationSentEvent implements ShouldBroadcast
     public function broadcastWith(): array
     {
         $userLocale = $this->notification->user?->locale ?? config('app.locale', 'en');
-
         app()->setLocale($userLocale);
 
-        return [
-            'notification' => (new NotificationResource($this->notification))->resolve(),
-        ];
+            return [
+                'notification' => (new NotificationResource($this->notification))->resolve(),
+            ];
     }
 }
--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -11,12 +11,12 @@ class NotificationResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'type' => $this->type,
-            'title' => __($this->title_key, $this->parameters ?? []),
-            'body' => __($this->body_key, $this->parameters ?? []),
-            'is_read' => (bool) $this->is_read,
-            'metadata' => $this->metadata,
+            'id'         => $this->id,
+            'type'       => $this->type,
+            'title'      => __($this->title_key, $this->parameters ?? []),
+            'body'       => __($this->body_key, $this->parameters ?? []),
+            'is_read'    => (bool) $this->is_read,
+            'metadata'   => $this->metadata,
             'created_at' => $this->created_at?->toDateTimeString(),
         ];
     }
