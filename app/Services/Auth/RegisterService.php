f4aeed91cb58488e626970df93d84732bdfe6129
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-09-17 16:57:30 +0300

    fix(register): ensure user roles are assigned and loaded correctly in registration response

--- app/Services/Auth/RegisterService.php
+++ app/Services/Auth/RegisterService.php
@@ -24,6 +24,8 @@ class RegisterService
             'role' => 'Owner',
         ]);
 
+        $user->assignRole('Owner');
+
         // Generate OTP and send it to the registered email
         $this->otpService->generate($user->email);
 
@@ -34,7 +36,7 @@ class RegisterService
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() / 60 .' hours',
-            'user' => $user,
+            'user' => $user->load('roles', 'permissions'),
         ];
 
         // $otp = $this->otpService->generate($user->email);
